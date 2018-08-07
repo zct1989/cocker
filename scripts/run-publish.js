0bc8d635d30310801aa128db6fdce47c45ab7222
zhuchentong <2037630@163.com>
2018-08-07 19:13:48 +0800

    udpate

---
+++ scripts/run-publish.js
@@ -0,0 +1,16 @@
+const shelljs = require('shelljs')
+const path = require('path')
+let root = path.resolve(__dirname, '..')
+
+shelljs.cp(".publishrc", "./bundle/common/")
+shelljs.cp(".publishrc", "./bundle/core/")
+shelljs.cp(".publishrc", "./bundle/extend/")
+
+shelljs.cd(path.join(root, 'bundle', 'common'))
+let child = shelljs.exec("npx publish-please")
+
+// shelljs.cd(path.join(root, 'bundle', 'core'))
+// shelljs.exec("npm run publish-please")
+
+shelljs.cd(path.join(root, 'bundle', 'extend'))
+shelljs.exec("npm run publish-please")
